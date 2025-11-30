afc61c531db6e2c858aa5543ec13a298cc96040c
kamail2014876-gif <kamail2014876@gmail.com>
2025-11-30 18:21:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,24 @@ def text_event (message) :
         bot. send_message(message.chat.id, message. text)
 
 
+if __name__ == "__main__":
+    server_url = os.getenv("RENDER_EXTERNAL_URL")
+    if server_url and TOKEN:
+        webhook_url = f"{server_url}/{TOKEN}"
+        set_webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={webhook_url}"
+        try:
+            r = requests.get(set_webhook_url)
+            print("Webhook установлен:", r.text)
+        except Exception as e:
+            print("Ошибка при установке webhook:", e)
+
+        port = int(os.environ.get("PORT", 10000))
+        print(f"Starting server on port {port}")
+        app.run(host='0.0.0.0', port=port)
+    else:
+        print("Запуск бота в режиме pooling")
+        bot.remove_webhook()
+        bot.polling(none_stop=True)
 
 
 
